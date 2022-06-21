fb24dddecf90f78918bd8633863cede5df35fc9d
louiejay00 <louie.jay.becudo.99@gmail.com>
2022-06-21 22:27:17 +0800

    master

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,79 @@
-import { Typography } from "@material-ui/core";
-
+import { Typography,Button } from "@material-ui/core";
+import axios from "axios";
 export default function IndexPage() {
+
+  const handleBackup = () =>{   
+    console.log("clicked");  
+    axios
+    .get("/admin/databasebackup", {
+    })
+    .then((res) => {
+      console.log("res", res);
+      console.log("res", res.data);
+    })
+    .catch((err) => {
+      console.error("error in log Creation", err);
+    })
+
+  };  
+
+const handleRestore = () =>{
+  console.log("handleRestore clicke");
+  axios
+  .get("/admin/databaserestore", {
+  })
+  .then((res) => {
+    console.log("res", res);
+    console.log("res", res.data);
+  })
+  .catch((err) => {
+    console.error("error in log Creation", err);
+  })
+}
+
+   
   return (
-    <>
+    <>    
+         <Button
+      onClick= {handleBackup}
+          variant="contained"  
+          style={{
+            left: "75%",
+            borderRadius: 15,
+            backgroundColor: "#fff",
+            marginRight:"10px",
+            zIndex:100
+          }}
+        >
+          <Typography
+            className="butt"
+            style={{
+              fontWeight: 400,
+            }}
+            variant="body1"
+          >
+           DB Backup
+          </Typography>
+        </Button>
+        <Button
+      onClick= {handleRestore}
+          variant="contained"
+          style={{
+            left: "75%",
+            borderRadius: 15,
+            backgroundColor: "#fff",
+          }}
+        >
+          <Typography
+            className="butt"
+            style={{
+              fontWeight: 400,
+            }}
+            variant="body1"
+          >
+            Restore Backup
+          </Typography>
+        </Button>
       <Typography paragraph>
         E-tulod ride hailing application that is convinient for people who don't have go
         to the toda station or wait for other passengers.  E-Tulod will be a mobile application 
@@ -10,7 +81,7 @@ export default function IndexPage() {
         available for pick up (after a passenger inputs the details in the app)
         and they will have the option to pick up the passenger after the request is 
         accepted the passenger will be notified that there is a tricycle on the way.  
-
+       
       </Typography>
       <Typography paragraph>
         This project is a different take on other ride hailing mobile applications 
